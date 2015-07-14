1ffeb3035198cb777e4cae58424bb4c6be2eb6df
Matthew Clark <mclark4386@gmail.com>
2015-07-14 02:20:39 -0500

    Translating Original

--- Chocorep/Properties/AssemblyInfo.cs
+++ Chocorep/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
+// General Information about an assembly is controlled through the following set of attributes.
+// If you want to change the information that is associated with the assembly,
+// Please change these attribute values.
 [assembly: AssemblyTitle("Chocorep(FF14 A Realm Reborn)")]
-[assembly: AssemblyDescription("FINAL FANTASY XIV - A Realm Reborn 用 Repソフト")]
+[assembly: AssemblyDescription("FINAL FANTASY XIV - Rep software for A Realm Reborn")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Chiro&Choco")]
 [assembly: AssemblyProduct("Chocorep for FF14 (A Realm Reborn)")]
@@ -14,23 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
-// その型の ComVisible 属性を true に設定してください。
+//When ComVisible the set to false, access from the COM component in this assembly
+// will be impossible. When accessing this assembly from the a COM component,
+// please set the ComVisible attribute of that type to true.
 [assembly: ComVisible(false)]
 
-// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+// The following GUID is in the case this project is exposed to COM
 [assembly: Guid("4c463d49-ec9b-4ec6-983b-99b9c62877b5")]
 
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+// Version information of the assembly, consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// Major Version
+// Minor Version
+// Build Number
+// Revision
 //
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
-// 既定値にすることができます:
-// [assembly: AssemblyVersion("1.0.*")]
+// Either specify all the values, the build and revision number using the '*' as shown below
+// You can be the default value:
+// [Assembly: AssemblyVersion ("1.0 *.")]
 [assembly: AssemblyVersion("2.1.1.3")]
 [assembly: AssemblyFileVersion("2.1.1.3")]
